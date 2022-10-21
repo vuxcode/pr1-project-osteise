3709c927d30994a9a092f8f7cb2a21cadc956bd2
osteise <oskarmellgren@live.se>
2022-10-21 12:34:42 +0200

    test

---
+++ questionOne.js
@@ -0,0 +1,7 @@
+
+function questOne() {
+    var question = document.write("<p>Wich song is not made by Michael Jackson</p>");
+
+    var answers = ["Thriller", "Billie Jean", "Born to Run", "Beat It"];
+    var correctAnswer = [3]
+}
