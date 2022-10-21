f9a67c942d2761326a60e74c9da40be7175280de
osteise <oskarmellgren@live.se>
2022-10-21 16:45:12 +0200

    updated options to take from js arrays

--- questionOne.js
+++ questionOne.js
@@ -1,14 +1,24 @@
-
+// create variables
 var question = document.getElementById("qOneInner");
-var options = document.getElementById("optOneInner");
+var optA = document.getElementById("optAInner");
+var optB = document.getElementById("optBInner");
+var optC = document.getElementById("optCInner");
+var optD = document.getElementById("optDInner");
 
-
+// create a function
 function questOne() {
-    var example = ("What song is not made by Michael Jackson?");
-
+    // create the question output
+    var q1 = ("What song is not made by Michael Jackson?");
+    question.innerText=q1;
+    // create the option outpus
     var opt1 = ["Thriller", "Billie Jean", "Born to Run", "Beat It"];
-    var answer = [3];
-    question.innerText=example;
+    optA.innerText=opt1[0];
+    optB.innerText=opt1[1];
+    optC.innerText=opt1[2];
+    optD.innerText=opt1[3];
+    
+    var correctAnswer = opt1[2];
+    
 }
 
 questOne();
