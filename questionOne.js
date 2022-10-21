154235ee1dd7096df6a36fe96576a100d46af351
osteise <oskarmellgren@live.se>
2022-10-21 19:01:43 +0200

    added another question

--- questionOne.js
+++ questionOne.js
@@ -5,6 +5,7 @@ var optB = document.getElementById("optBInner");
 var optC = document.getElementById("optCInner");
 var optD = document.getElementById("optDInner");
 
+
 // create a function
 function questOne() {
     // create the question output
@@ -17,8 +18,35 @@ function questOne() {
     optC.innerText=opt1[2];
     optD.innerText=opt1[3];
     
-    var correctAnswer = opt1[2];
     
-}
+    opt1[0,1,3] = false;
+    opt1[2] = true;
+    
+} 
 
-questOne();
+
+function questTwo() {
+    // create the question output
+    var q1 = ("What song is not made by The Bealtes?");
+    question.innerText=q1;
+    // create the option outpus
+    var opt1 = ["Yesterday", "Let It Be", "Here Comes the Sun", "Imagine"];
+    optA.innerText=opt1[0];
+    optB.innerText=opt1[1];
+    optC.innerText=opt1[2];
+    optD.innerText=opt1[3];
+    
+    
+    opt1[0,1,2] = false;
+    opt1[3] = true;
+    
+} 
+
+questTwo();
+
+function checkAnswer() {
+    questTwo();
+    //var opt1 = document.getElementById("optAInner");
+    console.log("HEJ");
+    console.log(opt1[index]);
+}
