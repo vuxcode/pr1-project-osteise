62a8a86031ffd433a8d2b9b7a6c50053d5e5982a
osteise <oskarmellgren@live.se>
2022-10-26 00:53:18 +0200

    managed to sort some problems
    changed the checkanswer function to use the optA variables instead of the opt1 array.
    Also created different functions for all the option buttons.

--- questionOne.js
+++ questionOne.js
@@ -1,4 +1,4 @@
-// create variables
+// create variables and add the buttons
 var question = document.getElementById("qOneInner");
 var optA = document.getElementById("optAInner");
 var optB = document.getElementById("optBInner");
@@ -6,48 +6,85 @@ var optC = document.getElementById("optCInner");
 var optD = document.getElementById("optDInner");
 
 
-// create a function
+// create a function to output question number one
 function questOne() {
     // create the question output
     var q1 = ("What song is not made by Michael Jackson?");
     question.innerText=q1;
-    // create the option outpus
+    // create the option outputs
     var opt1 = ["Thriller", "Billie Jean", "Born to Run", "Beat It"];
     optA.innerText=opt1[0];
     optB.innerText=opt1[1];
     optC.innerText=opt1[2];
     optD.innerText=opt1[3];
     
-    
-    opt1[0,1,3] = false;
-    opt1[2] = true;
+
+    // change the variables to the correct/incorrect answers
+    optA = false;
+    optB = false;
+    optC = true;
+    optD = false;
     
 } 
 
-
+// create function to output question number two
 function questTwo() {
     // create the question output
     var q1 = ("What song is not made by The Bealtes?");
     question.innerText=q1;
-    // create the option outpus
+    // create the option outputs
     var opt1 = ["Yesterday", "Let It Be", "Here Comes the Sun", "Imagine"];
     optA.innerText=opt1[0];
     optB.innerText=opt1[1];
     optC.innerText=opt1[2];
     optD.innerText=opt1[3];
     
-    
-    opt1[0,1,2] = false;
-    opt1[3] = true;
-    
-    console.log(opt1[index]);
+    // change the variable to the correct/incorrect answers
+    optA = false;
+    optB = false;
+    optC = false;
+    optD = true;
 } 
 
+// control wich question will be asked
 questTwo();
 
-function checkAnswer() {
-    questTwo();
-    //var opt1 = document.getElementById("optAInner");
-    console.log("HEJ");
-    console.log(opt1[index]);
+// create function to check option A
+function checkAnswerA() {
+    if (optA == false) {
+        console.log("That's incorrect..");
+    }
+    else {
+    console.log("That's correct!");
+    }
+}
+
+// create function to check option B
+function checkAnswerB() {
+    if (optB == false) {
+        console.log("That's incorrect..");
+    }
+    else {
+    console.log("That's correct!");
+    }
+}
+
+// create function to check option C
+function checkAnswerC() {
+    if (optC == false) {
+        console.log("That's incorrect..");
+    }
+    else {
+        console.log("That's correct!");
+    }
+}
+
+// create function to check option D
+function checkAnswerD() {
+    if (optD == false) {
+        console.log("That's incorrect..");
+    }
+    else {
+        console.log("That's correct!");
+    }
 }
