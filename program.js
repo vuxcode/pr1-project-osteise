24f25ed594ee47d0fe692b5b9e6da3e9039c6375
osteise <oskarmellgren@live.se>
2022-12-05 23:03:52 +0100

    added dark mode and fixed bug
    imported dark mode from web developement project. Also fixed a bug where the canvas timer stayed red for the next question if the time was running low on the previous one.

--- program.js
+++ program.js
@@ -39,7 +39,7 @@ function abort() {
     document.getElementById("fiftyfifty").disabled = true;
     document.getElementById("hint").disabled = true;
     // change the text on the submitbutton
-    document.getElementById("submitInner").innerText = "TIME IS UP!\nPress here to Play again!";
+    document.getElementById("submitInner").innerText = "TIME IS UP!\nYou had " + score + " correct answers. Press here to Play again!";
     // enable the submitbutton
     document.getElementById("submit").disabled = false;
     // change the color of the enable button to red
@@ -2398,6 +2398,9 @@ function updateX() {
     if (x > 600) {
         ctx.fillStyle = '#AE2021';
     }
+    if (x < 600) {
+        ctx.fillStyle = '#0077B8';
+    }
     // draw the line
     ctx.fillRect(0, 0, x, 150);
     // update the x variable
@@ -2405,3 +2408,67 @@ function updateX() {
 }
 // use the update function every second at 30fps
 var canvasTimer = setInterval(updateX, 1000);
+
+
+// target the mode button element
+var modeBtn = document.getElementById("mode");
+// set the default text to darkmode
+modeBtn.innerText="darkmode";
+// create a variable to the program knows what text to output
+var darkmodeOn = true;
+// target the body 
+var element = document.body;
+
+// create a variable to store what theme is being used
+var modeSet;
+// set the local storage
+// localStorage.setItem("saved_theme");
+// get the variable from the local storage
+modeSet = localStorage.getItem("saved_theme");
+
+function defaultTheme() {
+// create an if statement to know what theme to load
+if (modeSet == "dark") {
+    modeBtn.innerText="LIGHT";
+    darkmodeOn = false;
+    element.classList = "dark-mode";
+}
+else if (modeSet == "light") {
+    modeBtn.innerText="DARK";
+    darkmodeOn = true;
+    element.classList = "light-mode";
+}
+}
+
+defaultTheme();
+
+// create a function to swap to and from darkmode
+function darkmode() {
+    // create if statement to toggle dark mode on/off
+    // turn darkmode on
+    if (darkmodeOn) {
+        // set theme to dark
+        element.classList = "dark-mode";
+        // change the button text
+        modeBtn.innerText="LIGHT";
+        // set variable to false
+        darkmodeOn = false;
+        // set the local storage variable
+        modeSet = "dark";
+        // set it in the local storage
+        localStorage.setItem("saved_theme", modeSet);
+    }
+    // turn lightmode on
+    else if (darkmodeOn == false) {
+        // set the theme to light
+        element.classList = "light-mode";
+        // change the button text
+        modeBtn.innerText="DARK";
+        // set the variable to true
+        darkmodeOn = true;
+        // set the local storage variable
+        modeSet = "light";
+        // set it in the local storage
+        localStorage.setItem("saved_theme", modeSet);
+    }
+}
